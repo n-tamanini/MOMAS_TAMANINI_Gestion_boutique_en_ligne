66d36d2002c4cde59117cdf76c3a0fea248488fc
n-tamanini <n.tamanini@net.estia.fr>
2020-09-04 16:39:55 +0200

    génération script sql

--- bdd_eshop/script_bdd_eshop.sql
+++ bdd_eshop/script_bdd_eshop.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* Nom de SGBD :  ORACLE Version 10gR2                          */
-/* Date de création :  03/09/2020 12:15:19                      */
+/* Date de création :  04/09/2020 16:18:55                      */
 /*==============================================================*/
 
 
@@ -14,16 +14,16 @@ alter table COMMANDE
    drop constraint FK_COMMANDE_PASSE_UTILISAT;
 
 alter table CONTIENT_COULEUR
-   drop constraint FK_CONTIENT_CONTIENT_CATEGORI;
+   drop constraint FK_CONTIENT_COULEUR;
 
 alter table CONTIENT_COULEUR
-   drop constraint FK_CONTIENT_CONTIENT2_COULEUR;
+   drop constraint FK_CONTIENT_CONTIENT__COULEUR;
 
 alter table CONTIENT_TAILLE
-   drop constraint FK_CONTIENT_CONTIENT3_CATEGORI;
+   drop constraint FK_CONTIENT_CONTIENT__CATEGORI;
 
 alter table CONTIENT_TAILLE
-   drop constraint FK_CONTIENT_CONTIENT4_TAILLE;
+   drop constraint FK_CONTIENT_CONTIENT__TAILLE;
 
 alter table EST_COMMANDE
    drop constraint FK_EST_COMM_EST_COMMA_VETEMENT;
@@ -52,15 +52,15 @@ drop index PASSE_FK;
 
 drop table COMMANDE cascade constraints;
 
-drop index CONTIENT2_FK;
+drop index CONTIENT_COULEUR2_FK;
 
-drop index CONTIENT_FK;
+drop index CONTIENT_COULEUR_FK;
 
 drop table CONTIENT_COULEUR cascade constraints;
 
-drop index CONTIENT4_FK;
+drop index CONTIENT_TAILLE2_FK;
 
-drop index CONTIENT3_FK;
+drop index CONTIENT_TAILLE_FK;
 
 drop table CONTIENT_TAILLE cascade constraints;
 
@@ -88,7 +88,8 @@ drop table VETEMENT cascade constraints;
 /* Table : CATEGORIE                                            */
 /*==============================================================*/
 create table CATEGORIE  (
-   ID_CATEGORIE         NUMBER(4)                       not null,
+   ID_CATEGORIE         NUMBER(4)                       not null
+      constraint CKC_ID_CATEGORIE_CATEGORI check (ID_CATEGORIE >= 1),
    NOM_CATEGORIE        VARCHAR2(30)                    not null,
    constraint PK_CATEGORIE primary key (ID_CATEGORIE)
 );
@@ -97,8 +98,10 @@ create table CATEGORIE  (
 /* Table : CHOISIT                                              */
 /*==============================================================*/
 create table CHOISIT  (
-   ID_VETEMENT          NUMBER(4)                       not null,
-   ID_UTILISATEUR       NUMBER(4)                       not null,
+   ID_VETEMENT          NUMBER(4)                       not null
+      constraint CKC_ID_VETEMENT_CHOISIT check (ID_VETEMENT >= 1),
+   ID_UTILISATEUR       NUMBER(4)                       not null
+      constraint CKC_ID_UTILISATEUR_CHOISIT check (ID_UTILISATEUR >= 1),
    constraint PK_CHOISIT primary key (ID_VETEMENT, ID_UTILISATEUR)
 );
 
@@ -120,10 +123,15 @@ create index CHOISIT2_FK on CHOISIT (
 /* Table : COMMANDE                                             */
 /*==============================================================*/
 create table COMMANDE  (
-   ID_COMMANDE          NUMBER(4)                       not null,
-   ID_UTILISATEUR       NUMBER(4)                       not null,
+   ID_COMMANDE          NUMBER(4)                       not null
+      constraint CKC_ID_COMMANDE_COMMANDE check (ID_COMMANDE >= 1),
+   ID_UTILISATEUR       NUMBER(4)                       not null
+      constraint CKC_ID_UTILISATEUR_COMMANDE check (ID_UTILISATEUR >= 1),
    ETAT_COMMANDE        VARCHAR2(30)                    not null,
-   PRIX_COMMANDE        NUMBER                          not null,
+   PRIX_COMMANDE        NUMBER                          not null
+      constraint CKC_PRIX_COMMANDE_COMMANDE check (PRIX_COMMANDE between 2 and 2000),
+   QUANTITE_COMMANDEE   NUMBER                          not null
+      constraint CKC_QUANTITE_COMMANDE_COMMANDE check (QUANTITE_COMMANDEE between 1 and 20),
    constraint PK_COMMANDE primary key (ID_COMMANDE)
 );
 
@@ -138,22 +146,24 @@ create index PASSE_FK on COMMANDE (
 /* Table : CONTIENT_COULEUR                                     */
 /*==============================================================*/
 create table CONTIENT_COULEUR  (
-   ID_CATEGORIE         NUMBER(4)                       not null,
-   ID_COULEUR           NUMBER(4)                       not null,
-   constraint PK_CONTIENT_COULEUR primary key (ID_CATEGORIE, ID_COULEUR)
+   CATEGORIE_ID         NUMBER(4)                       not null
+      constraint CKC_CATEGORIE_ID_CONTIENT check (CATEGORIE_ID >= 1),
+   ID_COULEUR           NUMBER(4)                       not null
+      constraint CKC_ID_COULEUR_CONTIENT check (ID_COULEUR >= 1),
+   constraint PK_CONTIENT_COULEUR primary key (CATEGORIE_ID, ID_COULEUR)
 );
 
 /*==============================================================*/
-/* Index : CONTIENT_FK                                          */
+/* Index : CONTIENT_COULEUR_FK                                  */
 /*==============================================================*/
-create index CONTIENT_FK on CONTIENT_COULEUR (
-   ID_CATEGORIE ASC
+create index CONTIENT_COULEUR_FK on CONTIENT_COULEUR (
+   CATEGORIE_ID ASC
 );
 
 /*==============================================================*/
-/* Index : CONTIENT2_FK                                         */
+/* Index : CONTIENT_COULEUR2_FK                                 */
 /*==============================================================*/
-create index CONTIENT2_FK on CONTIENT_COULEUR (
+create index CONTIENT_COULEUR2_FK on CONTIENT_COULEUR (
    ID_COULEUR ASC
 );
 
@@ -161,23 +171,24 @@ create index CONTIENT2_FK on CONTIENT_COULEUR (
 /* Table : CONTIENT_TAILLE                                      */
 /*==============================================================*/
 create table CONTIENT_TAILLE  (
-   ID_CATEGORIE         NUMBER(4)                       not null,
+   ID_CATEGORIE         NUMBER(4)                       not null
+      constraint CKC_ID_CATEGORIE_CONTIENT check (ID_CATEGORIE >= 1),
    ID_TAILLE            NUMBER(4)                       not null
       constraint CKC_ID_TAILLE_CONTIENT check (ID_TAILLE between 1 and 6),
    constraint PK_CONTIENT_TAILLE primary key (ID_CATEGORIE, ID_TAILLE)
 );
 
 /*==============================================================*/
-/* Index : CONTIENT3_FK                                         */
+/* Index : CONTIENT_TAILLE_FK                                   */
 /*==============================================================*/
-create index CONTIENT3_FK on CONTIENT_TAILLE (
+create index CONTIENT_TAILLE_FK on CONTIENT_TAILLE (
    ID_CATEGORIE ASC
 );
 
 /*==============================================================*/
-/* Index : CONTIENT4_FK                                         */
+/* Index : CONTIENT_TAILLE2_FK                                  */
 /*==============================================================*/
-create index CONTIENT4_FK on CONTIENT_TAILLE (
+create index CONTIENT_TAILLE2_FK on CONTIENT_TAILLE (
    ID_TAILLE ASC
 );
 
@@ -185,7 +196,8 @@ create index CONTIENT4_FK on CONTIENT_TAILLE (
 /* Table : COULEUR                                              */
 /*==============================================================*/
 create table COULEUR  (
-   ID_COULEUR           NUMBER(4)                       not null,
+   ID_COULEUR           NUMBER(4)                       not null
+      constraint CKC_ID_COULEUR_COULEUR check (ID_COULEUR >= 1),
    NOM_COULEUR          VARCHAR2(30),
    constraint PK_COULEUR primary key (ID_COULEUR)
 );
@@ -194,8 +206,10 @@ create table COULEUR  (
 /* Table : EST_COMMANDE                                         */
 /*==============================================================*/
 create table EST_COMMANDE  (
-   ID_VETEMENT          NUMBER(4)                       not null,
-   ID_COMMANDE          NUMBER(4)                       not null,
+   ID_VETEMENT          NUMBER(4)                       not null
+      constraint CKC_ID_VETEMENT_EST_COMM check (ID_VETEMENT >= 1),
+   ID_COMMANDE          NUMBER(4)                       not null
+      constraint CKC_ID_COMMANDE_EST_COMM check (ID_COMMANDE >= 1),
    constraint PK_EST_COMMANDE primary key (ID_VETEMENT, ID_COMMANDE)
 );
 
@@ -227,7 +241,8 @@ create table TAILLE  (
 /* Table : UTILISATEUR                                          */
 /*==============================================================*/
 create table UTILISATEUR  (
-   ID_UTILISATEUR       NUMBER(4)                       not null,
+   ID_UTILISATEUR       NUMBER(4)                       not null
+      constraint CKC_ID_UTILISATEUR_UTILISAT check (ID_UTILISATEUR >= 1),
    NOM_UTILISATEUR      CHAR(30)                        not null,
    PRENOM_UTILISATEUR   VARCHAR2(30)                    not null,
    ADRESSE_UTILISATEUR  VARCHAR2(80)                    not null,
@@ -239,14 +254,20 @@ create table UTILISATEUR  (
 /* Table : VETEMENT                                             */
 /*==============================================================*/
 create table VETEMENT  (
-   ID_VETEMENT          NUMBER(4)                       not null,
-   ID_COULEUR           NUMBER(4)                       not null,
+   ID_VETEMENT          NUMBER(4)                       not null
+      constraint CKC_ID_VETEMENT_VETEMENT check (ID_VETEMENT >= 1),
+   ID_COULEUR           NUMBER(4)                       not null
+      constraint CKC_ID_COULEUR_VETEMENT check (ID_COULEUR >= 1),
    ID_TAILLE            NUMBER(4)                       not null
       constraint CKC_ID_TAILLE_VETEMENT check (ID_TAILLE between 1 and 6),
-   ID_CATEGORIE         NUMBER(4)                       not null,
+   ID_CATEGORIE         NUMBER(4)                       not null
+      constraint CKC_ID_CATEGORIE_VETEMENT check (ID_CATEGORIE >= 1),
    NOM_VETEMENT         VARCHAR2(30)                    not null,
    DESCRIPTION_VETEMENT CLOB                            not null,
-   PRIX_VETEMENT        NUMBER                          not null,
+   PRIX_VETEMENT        NUMBER                          not null
+      constraint CKC_PRIX_VETEMENT_VETEMENT check (PRIX_VETEMENT between 2 and 300),
+   QUANTITE_VETEMENT    NUMBER(4)                       not null
+      constraint CKC_QUANTITE_VETEMENT_VETEMENT check (QUANTITE_VETEMENT >= 0),
    constraint PK_VETEMENT primary key (ID_VETEMENT)
 );
 
@@ -284,19 +305,19 @@ alter table COMMANDE
       references UTILISATEUR (ID_UTILISATEUR);
 
 alter table CONTIENT_COULEUR
-   add constraint FK_CONTIENT_CONTIENT_CATEGORI foreign key (ID_CATEGORIE)
+   add constraint FK_CONTIENT_COULEUR foreign key (CATEGORIE_ID)
       references CATEGORIE (ID_CATEGORIE);
 
 alter table CONTIENT_COULEUR
-   add constraint FK_CONTIENT_CONTIENT2_COULEUR foreign key (ID_COULEUR)
+   add constraint FK_CONTIENT_CONTIENT__COULEUR foreign key (ID_COULEUR)
       references COULEUR (ID_COULEUR);
 
 alter table CONTIENT_TAILLE
-   add constraint FK_CONTIENT_CONTIENT3_CATEGORI foreign key (ID_CATEGORIE)
+   add constraint FK_CONTIENT_CONTIENT__CATEGORI foreign key (ID_CATEGORIE)
       references CATEGORIE (ID_CATEGORIE);
 
 alter table CONTIENT_TAILLE
-   add constraint FK_CONTIENT_CONTIENT4_TAILLE foreign key (ID_TAILLE)
+   add constraint FK_CONTIENT_CONTIENT__TAILLE foreign key (ID_TAILLE)
       references TAILLE (ID_TAILLE);
 
 alter table EST_COMMANDE
