21e55ac20c4ab64410143fd2a0b5b8dcd3375d76
n-tamanini <n.tamanini@net.estia.fr>
2020-09-08 14:02:16 +0200

    modification MCD/MLD, regénération du script SQL et ajout manuel des "AUTO_INCREMENT"

--- bdd_eshop/script_bdd_eshop.sql
+++ bdd_eshop/script_bdd_eshop.sql
@@ -1,11 +1,11 @@
 /*==============================================================*/
 /* Nom de SGBD :  ORACLE Version 10gR2                          */
-/* Date de création :  04/09/2020 16:18:55                      */
+/* Date de création :  08/09/2020 13:54:21                      */
 /*==============================================================*/
 
 
 alter table CHOISIT
-   drop constraint FK_CHOISIT_CHOISIT_VETEMENT;
+   drop constraint FK_CHOISIT_CHOISIT_DECLINAI;
 
 alter table CHOISIT
    drop constraint FK_CHOISIT_CHOISIT2_UTILISAT;
@@ -13,32 +13,23 @@ alter table CHOISIT
 alter table COMMANDE
    drop constraint FK_COMMANDE_PASSE_UTILISAT;
 
-alter table CONTIENT_COULEUR
-   drop constraint FK_CONTIENT_COULEUR;
+alter table DECLINAISON_VETEMENT
+   drop constraint FK_DECLINAI_EST_DECLI_VETEMENT;
 
-alter table CONTIENT_COULEUR
-   drop constraint FK_CONTIENT_CONTIENT__COULEUR;
+alter table DECLINAISON_VETEMENT
+   drop constraint FK_DECLINAI_EST_DECLI_TAILLE;
 
-alter table CONTIENT_TAILLE
-   drop constraint FK_CONTIENT_CONTIENT__CATEGORI;
-
-alter table CONTIENT_TAILLE
-   drop constraint FK_CONTIENT_CONTIENT__TAILLE;
+alter table DECLINAISON_VETEMENT
+   drop constraint FK_DECLINAI_SE_DECLIN_COULEUR;
 
 alter table EST_COMMANDE
-   drop constraint FK_EST_COMM_EST_COMMA_VETEMENT;
+   drop constraint FK_EST_COMM_EST_COMMA_DECLINAI;
 
 alter table EST_COMMANDE
    drop constraint FK_EST_COMM_EST_COMMA_COMMANDE;
 
 alter table VETEMENT
    drop constraint FK_VETEMENT_APPARTIEN_CATEGORI;
-
-alter table VETEMENT
-   drop constraint FK_VETEMENT_EST_ASSOC_COULEUR;
-
-alter table VETEMENT
-   drop constraint FK_VETEMENT_EST_DECLI_TAILLE;
 
 drop table CATEGORIE cascade constraints;
 
@@ -52,19 +43,15 @@ drop index PASSE_FK;
 
 drop table COMMANDE cascade constraints;
 
-drop index CONTIENT_COULEUR2_FK;
-
-drop index CONTIENT_COULEUR_FK;
-
-drop table CONTIENT_COULEUR cascade constraints;
-
-drop index CONTIENT_TAILLE2_FK;
-
-drop index CONTIENT_TAILLE_FK;
-
-drop table CONTIENT_TAILLE cascade constraints;
-
 drop table COULEUR cascade constraints;
+
+drop index EST_DECLINE_FK;
+
+drop index SE_DECLINE_EN_FK;
+
+drop index EST_DECLINE_EN_FK;
+
+drop table DECLINAISON_VETEMENT cascade constraints;
 
 drop index EST_COMMANDE2_FK;
 
@@ -76,10 +63,6 @@ drop table TAILLE cascade constraints;
 
 drop table UTILISATEUR cascade constraints;
 
-drop index EST_DECLINE_EN_FK;
-
-drop index EST_ASSOCIE_A_FK;
-
 drop index APPARTIENT_A_FK;
 
 drop table VETEMENT cascade constraints;
@@ -88,7 +71,7 @@ drop table VETEMENT cascade constraints;
 /* Table : CATEGORIE                                            */
 /*==============================================================*/
 create table CATEGORIE  (
-   ID_CATEGORIE         NUMBER(4)                       not null
+   ID_CATEGORIE         NUMBER(4)       not null      AUTO_INCREMENT
       constraint CKC_ID_CATEGORIE_CATEGORI check (ID_CATEGORIE >= 1),
    NOM_CATEGORIE        VARCHAR2(30)                    not null,
    constraint PK_CATEGORIE primary key (ID_CATEGORIE)
@@ -98,18 +81,18 @@ create table CATEGORIE  (
 /* Table : CHOISIT                                              */
 /*==============================================================*/
 create table CHOISIT  (
-   ID_VETEMENT          NUMBER(4)                       not null
-      constraint CKC_ID_VETEMENT_CHOISIT check (ID_VETEMENT >= 1),
+   ID_DECLINAISON       NUMBER(4)         not null     AUTO_INCREMENT
+      constraint CKC_ID_DECLINAISON_CHOISIT check (ID_DECLINAISON >= 1),
    ID_UTILISATEUR       NUMBER(4)                       not null
       constraint CKC_ID_UTILISATEUR_CHOISIT check (ID_UTILISATEUR >= 1),
-   constraint PK_CHOISIT primary key (ID_VETEMENT, ID_UTILISATEUR)
+   constraint PK_CHOISIT primary key (ID_DECLINAISON, ID_UTILISATEUR)
 );
 
 /*==============================================================*/
 /* Index : CHOISIT_FK                                           */
 /*==============================================================*/
 create index CHOISIT_FK on CHOISIT (
-   ID_VETEMENT ASC
+   ID_DECLINAISON ASC
 );
 
 /*==============================================================*/
@@ -123,15 +106,13 @@ create index CHOISIT2_FK on CHOISIT (
 /* Table : COMMANDE                                             */
 /*==============================================================*/
 create table COMMANDE  (
-   ID_COMMANDE          NUMBER(4)                       not null
+   ID_COMMANDE          NUMBER(4)      not null     AUTO_INCREMENT
       constraint CKC_ID_COMMANDE_COMMANDE check (ID_COMMANDE >= 1),
    ID_UTILISATEUR       NUMBER(4)                       not null
       constraint CKC_ID_UTILISATEUR_COMMANDE check (ID_UTILISATEUR >= 1),
    ETAT_COMMANDE        VARCHAR2(30)                    not null,
    PRIX_COMMANDE        NUMBER                          not null
       constraint CKC_PRIX_COMMANDE_COMMANDE check (PRIX_COMMANDE between 2 and 2000),
-   QUANTITE_COMMANDEE   NUMBER                          not null
-      constraint CKC_QUANTITE_COMMANDE_COMMANDE check (QUANTITE_COMMANDEE between 1 and 20),
    constraint PK_COMMANDE primary key (ID_COMMANDE)
 );
 
@@ -143,81 +124,73 @@ create index PASSE_FK on COMMANDE (
 );
 
 /*==============================================================*/
-/* Table : CONTIENT_COULEUR                                     */
-/*==============================================================*/
-create table CONTIENT_COULEUR  (
-   CATEGORIE_ID         NUMBER(4)                       not null
-      constraint CKC_CATEGORIE_ID_CONTIENT check (CATEGORIE_ID >= 1),
-   ID_COULEUR           NUMBER(4)                       not null
-      constraint CKC_ID_COULEUR_CONTIENT check (ID_COULEUR >= 1),
-   constraint PK_CONTIENT_COULEUR primary key (CATEGORIE_ID, ID_COULEUR)
-);
-
-/*==============================================================*/
-/* Index : CONTIENT_COULEUR_FK                                  */
-/*==============================================================*/
-create index CONTIENT_COULEUR_FK on CONTIENT_COULEUR (
-   CATEGORIE_ID ASC
-);
-
-/*==============================================================*/
-/* Index : CONTIENT_COULEUR2_FK                                 */
-/*==============================================================*/
-create index CONTIENT_COULEUR2_FK on CONTIENT_COULEUR (
-   ID_COULEUR ASC
-);
-
-/*==============================================================*/
-/* Table : CONTIENT_TAILLE                                      */
-/*==============================================================*/
-create table CONTIENT_TAILLE  (
-   ID_CATEGORIE         NUMBER(4)                       not null
-      constraint CKC_ID_CATEGORIE_CONTIENT check (ID_CATEGORIE >= 1),
-   ID_TAILLE            NUMBER(4)                       not null
-      constraint CKC_ID_TAILLE_CONTIENT check (ID_TAILLE between 1 and 6),
-   constraint PK_CONTIENT_TAILLE primary key (ID_CATEGORIE, ID_TAILLE)
-);
-
-/*==============================================================*/
-/* Index : CONTIENT_TAILLE_FK                                   */
-/*==============================================================*/
-create index CONTIENT_TAILLE_FK on CONTIENT_TAILLE (
-   ID_CATEGORIE ASC
-);
-
-/*==============================================================*/
-/* Index : CONTIENT_TAILLE2_FK                                  */
-/*==============================================================*/
-create index CONTIENT_TAILLE2_FK on CONTIENT_TAILLE (
-   ID_TAILLE ASC
-);
-
-/*==============================================================*/
 /* Table : COULEUR                                              */
 /*==============================================================*/
 create table COULEUR  (
-   ID_COULEUR           NUMBER(4)                       not null
+   ID_COULEUR           NUMBER(4)      not null    AUTO_INCREMENT
       constraint CKC_ID_COULEUR_COULEUR check (ID_COULEUR >= 1),
    NOM_COULEUR          VARCHAR2(30),
    constraint PK_COULEUR primary key (ID_COULEUR)
 );
 
 /*==============================================================*/
+/* Table : DECLINAISON_VETEMENT                                 */
+/*==============================================================*/
+create table DECLINAISON_VETEMENT  (
+   ID_DECLINAISON       NUMBER(4)        not null   AUTO_INCREMENT
+      constraint CKC_ID_DECLINAISON_DECLINAI check (ID_DECLINAISON >= 1),
+   ID_COULEUR           NUMBER(4)                       not null
+      constraint CKC_ID_COULEUR_DECLINAI check (ID_COULEUR >= 1),
+   ID_VETEMENT          NUMBER(4)                       not null
+      constraint CKC_ID_VETEMENT_DECLINAI check (ID_VETEMENT >= 1),
+   ID_TAILLE            NUMBER(4)                       not null
+      constraint CKC_ID_TAILLE_DECLINAI check (ID_TAILLE between 1 and 6),
+   PRIX_DECLINAISON     NUMBER                          not null
+      constraint CKC_PRIX_DECLINAISON_DECLINAI check (PRIX_DECLINAISON between 2 and 300),
+   QUANTITE_DECLINAISON NUMBER(4)                       not null
+      constraint CKC_QUANTITE_DECLINAI_DECLINAI check (QUANTITE_DECLINAISON >= 0),
+   constraint PK_DECLINAISON_VETEMENT primary key (ID_DECLINAISON)
+);
+
+/*==============================================================*/
+/* Index : EST_DECLINE_EN_FK                                    */
+/*==============================================================*/
+create index EST_DECLINE_EN_FK on DECLINAISON_VETEMENT (
+   ID_TAILLE ASC
+);
+
+/*==============================================================*/
+/* Index : SE_DECLINE_EN_FK                                     */
+/*==============================================================*/
+create index SE_DECLINE_EN_FK on DECLINAISON_VETEMENT (
+   ID_COULEUR ASC
+);
+
+/*==============================================================*/
+/* Index : EST_DECLINE_FK                                       */
+/*==============================================================*/
+create index EST_DECLINE_FK on DECLINAISON_VETEMENT (
+   ID_VETEMENT ASC
+);
+
+/*==============================================================*/
 /* Table : EST_COMMANDE                                         */
 /*==============================================================*/
 create table EST_COMMANDE  (
-   ID_VETEMENT          NUMBER(4)                       not null
-      constraint CKC_ID_VETEMENT_EST_COMM check (ID_VETEMENT >= 1),
+   ID_DECLINAISON       NUMBER(4)                       not null
+      constraint CKC_ID_DECLINAISON_EST_COMM check (ID_DECLINAISON >= 1),
    ID_COMMANDE          NUMBER(4)                       not null
       constraint CKC_ID_COMMANDE_EST_COMM check (ID_COMMANDE >= 1),
-   constraint PK_EST_COMMANDE primary key (ID_VETEMENT, ID_COMMANDE)
+   QUANTITE_COMMANDEE   NUMBER(3)                      
+      constraint CKC_QUANTITE_COMMANDE_EST_COMM check (QUANTITE_COMMANDEE is null or (QUANTITE_COMMANDEE between 1 and 20)),
+   constraint PK_EST_COMMANDE primary key (ID_DECLINAISON, ID_COMMANDE)
 );
 
 /*==============================================================*/
 /* Index : EST_COMMANDE_FK                                      */
 /*==============================================================*/
 create index EST_COMMANDE_FK on EST_COMMANDE (
-   ID_VETEMENT ASC
+   ID_DECLINAISON ASC
 );
 
 /*==============================================================*/
@@ -231,7 +204,7 @@ create index EST_COMMANDE2_FK on EST_COMMANDE (
 /* Table : TAILLE                                               */
 /*==============================================================*/
 create table TAILLE  (
-   ID_TAILLE            NUMBER(4)                       not null
+   ID_TAILLE            NUMBER(4)      not null       AUTO_INCREMENT
       constraint CKC_ID_TAILLE_TAILLE check (ID_TAILLE between 1 and 6),
    NOM_TAILLE           VARCHAR2(30)                    not null,
    constraint PK_TAILLE primary key (ID_TAILLE)
@@ -241,12 +214,13 @@ create table TAILLE  (
 /* Table : UTILISATEUR                                          */
 /*==============================================================*/
 create table UTILISATEUR  (
-   ID_UTILISATEUR       NUMBER(4)                       not null
+   ID_UTILISATEUR       NUMBER(4)    not null       AUTO_INCREMENT
       constraint CKC_ID_UTILISATEUR_UTILISAT check (ID_UTILISATEUR >= 1),
    NOM_UTILISATEUR      CHAR(30)                        not null,
    PRENOM_UTILISATEUR   VARCHAR2(30)                    not null,
    ADRESSE_UTILISATEUR  VARCHAR2(80)                    not null,
    EMAIL_UTILISATEUR    VARCHAR2(50)                    not null,
+   MDP_UTILISATEUR      VARCHAR2(30)                    not null,
    constraint PK_UTILISATEUR primary key (ID_UTILISATEUR)
 );
 
@@ -254,20 +228,12 @@ create table UTILISATEUR  (
 /* Table : VETEMENT                                             */
 /*==============================================================*/
 create table VETEMENT  (
-   ID_VETEMENT          NUMBER(4)                       not null
+   ID_VETEMENT          NUMBER(4)   not null       AUTO_INCREMENT
       constraint CKC_ID_VETEMENT_VETEMENT check (ID_VETEMENT >= 1),
-   ID_COULEUR           NUMBER(4)                       not null
-      constraint CKC_ID_COULEUR_VETEMENT check (ID_COULEUR >= 1),
-   ID_TAILLE            NUMBER(4)                       not null
-      constraint CKC_ID_TAILLE_VETEMENT check (ID_TAILLE between 1 and 6),
    ID_CATEGORIE         NUMBER(4)                       not null
       constraint CKC_ID_CATEGORIE_VETEMENT check (ID_CATEGORIE >= 1),
    NOM_VETEMENT         VARCHAR2(30)                    not null,
    DESCRIPTION_VETEMENT CLOB                            not null,
-   PRIX_VETEMENT        NUMBER                          not null
-      constraint CKC_PRIX_VETEMENT_VETEMENT check (PRIX_VETEMENT between 2 and 300),
-   QUANTITE_VETEMENT    NUMBER(4)                       not null
-      constraint CKC_QUANTITE_VETEMENT_VETEMENT check (QUANTITE_VETEMENT >= 0),
    constraint PK_VETEMENT primary key (ID_VETEMENT)
 );
 
@@ -278,23 +244,9 @@ create index APPARTIENT_A_FK on VETEMENT (
    ID_CATEGORIE ASC
 );
 
-/*==============================================================*/
-/* Index : EST_ASSOCIE_A_FK                                     */
-/*==============================================================*/
-create index EST_ASSOCIE_A_FK on VETEMENT (
-   ID_COULEUR ASC
-);
-
-/*==============================================================*/
-/* Index : EST_DECLINE_EN_FK                                    */
-/*==============================================================*/
-create index EST_DECLINE_EN_FK on VETEMENT (
-   ID_TAILLE ASC
-);
-
 alter table CHOISIT
-   add constraint FK_CHOISIT_CHOISIT_VETEMENT foreign key (ID_VETEMENT)
-      references VETEMENT (ID_VETEMENT);
+   add constraint FK_CHOISIT_CHOISIT_DECLINAI foreign key (ID_DECLINAISON)
+      references DECLINAISON_VETEMENT (ID_DECLINAISON);
 
 alter table CHOISIT
    add constraint FK_CHOISIT_CHOISIT2_UTILISAT foreign key (ID_UTILISATEUR)
@@ -304,25 +256,21 @@ alter table COMMANDE
    add constraint FK_COMMANDE_PASSE_UTILISAT foreign key (ID_UTILISATEUR)
       references UTILISATEUR (ID_UTILISATEUR);
 
-alter table CONTIENT_COULEUR
-   add constraint FK_CONTIENT_COULEUR foreign key (CATEGORIE_ID)
-      references CATEGORIE (ID_CATEGORIE);
+alter table DECLINAISON_VETEMENT
+   add constraint FK_DECLINAI_EST_DECLI_VETEMENT foreign key (ID_VETEMENT)
+      references VETEMENT (ID_VETEMENT);
 
-alter table CONTIENT_COULEUR
-   add constraint FK_CONTIENT_CONTIENT__COULEUR foreign key (ID_COULEUR)
-      references COULEUR (ID_COULEUR);
-
-alter table CONTIENT_TAILLE
-   add constraint FK_CONTIENT_CONTIENT__CATEGORI foreign key (ID_CATEGORIE)
-      references CATEGORIE (ID_CATEGORIE);
-
-alter table CONTIENT_TAILLE
-   add constraint FK_CONTIENT_CONTIENT__TAILLE foreign key (ID_TAILLE)
+alter table DECLINAISON_VETEMENT
+   add constraint FK_DECLINAI_EST_DECLI_TAILLE foreign key (ID_TAILLE)
       references TAILLE (ID_TAILLE);
 
+alter table DECLINAISON_VETEMENT
+   add constraint FK_DECLINAI_SE_DECLIN_COULEUR foreign key (ID_COULEUR)
+      references COULEUR (ID_COULEUR);
+
 alter table EST_COMMANDE
-   add constraint FK_EST_COMM_EST_COMMA_VETEMENT foreign key (ID_VETEMENT)
-      references VETEMENT (ID_VETEMENT);
+   add constraint FK_EST_COMM_EST_COMMA_DECLINAI foreign key (ID_DECLINAISON)
+      references DECLINAISON_VETEMENT (ID_DECLINAISON);
 
 alter table EST_COMMANDE
    add constraint FK_EST_COMM_EST_COMMA_COMMANDE foreign key (ID_COMMANDE)
@@ -331,12 +279,4 @@ alter table EST_COMMANDE
 alter table VETEMENT
    add constraint FK_VETEMENT_APPARTIEN_CATEGORI foreign key (ID_CATEGORIE)
       references CATEGORIE (ID_CATEGORIE);
-
-alter table VETEMENT
-   add constraint FK_VETEMENT_EST_ASSOC_COULEUR foreign key (ID_COULEUR)
-      references COULEUR (ID_COULEUR);
-
-alter table VETEMENT
-   add constraint FK_VETEMENT_EST_DECLI_TAILLE foreign key (ID_TAILLE)
-      references TAILLE (ID_TAILLE);
 
