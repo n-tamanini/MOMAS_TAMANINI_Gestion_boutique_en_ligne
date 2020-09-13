4b644566b4f8bdcecc34cc3dae0c3cc7ae84baf1
n-tamanini <n.tamanini@net.estia.fr>
2020-09-13 13:50:33 +0200

    ajout logos, titres ...

--- frontend_eshop/src/app/navigation/navigation.ts
+++ frontend_eshop/src/app/navigation/navigation.ts
@@ -2,25 +2,17 @@ import { FuseNavigation } from '@fuse/types';
 
 export const navigation: FuseNavigation[] = [
     {
-        id       : 'applications',
-        title    : 'Applications',
-        translate: 'NAV.APPLICATIONS',
-        type     : 'group',
-        children : [
-            {
-                id       : 'dashboard',
-                title    : 'Accueil',
-                type     : 'item',
-                icon     : 'store',
-                url      : '/dashboard',
-            },
-            {
-                id       : 'backoffice',
-                title    : 'Arrière boutique',
-                type     : 'item',
-                icon     : 'settings',
-                url      : '/backoffice',
-            },
-        ]
+        id: 'dashboard',
+        title: 'Accueil',
+        type: 'item',
+        icon: 'store',
+        url: '/dashboard',
+    },
+    {
+        id: 'backoffice',
+        title: 'Arrière boutique',
+        type: 'item',
+        icon: 'settings',
+        url: '/backoffice',
     }
 ];
